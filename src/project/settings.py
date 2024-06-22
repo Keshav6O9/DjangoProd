a01898f75693f78877a57be4fe3f1596eac1b162
Keshav vaishnav <66151299+Keshav6O9@users.noreply.github.com>
2024-06-22 13:17:10 +0000

    changed project structure

--- src/project/settings.py
+++ src/project/settings.py
@@ -24,9 +24,14 @@ SECRET_KEY = "django-insecure-+h4gxcn4f#0iw_q-ka$fwqe3-p+n!26&num1zplf^s-+5-80d(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
